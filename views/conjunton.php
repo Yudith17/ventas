cb953654cacd3f5b8016e61f5fa7d91983eafbf0
Yudith17 <tanirimachi@gmail.com>
2024-10-10 12:37:12 -0500

    a

--- views/conjunton.php
+++ views/conjunton.php
@@ -126,7 +126,7 @@
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfFipS63pBZH2k7cz_HNYXfQP0_oK1OI6_XA&s" class="card-img-top" alt="...">
                                 <div class="card-body">
                                   <h5 class="card-title">Card title</h5>
-                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's </p>
                                   <a href="detalle_carrito.html" class="btn btn-primary">Ver Detalles</a>
                                   <a href="carrito.html" class="btn btn-success">Añadir a Carrito</a>
                                 </div>
@@ -137,7 +137,7 @@
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHAqIJaoPlcxaTHjEPY48d9Seb5t3bW-7mWQ&s" class="card-img-top" alt="...">
                                 <div class="card-body">
                                   <h5 class="card-title">Card title</h5>
-                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's </p>
                                   <a href="detalle_carrito.html" class="btn btn-primary">Ver Detalles</a>
                                   <a href="carrito.html" class="btn btn-success">Añadir a Carrito</a>
                                 </div>
