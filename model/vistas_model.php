6a07dbb332c460c6201bce473a2786116bbf49c5
Yudith17 <tanirimachi@gmail.com>
2024-12-12 13:02:09 -0500

    1

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -8,10 +8,10 @@ class vistaModelo{
           'carrito','nosotros','detallepantalon','contacto','pagar','detalleproducto', 'registrar-categoria', 'registrar-compras',
            'registrar-persona','productos','compra','editar-producto','editar-categoria','editar-persona','panel']; //carpetas de html
 
-/*           if (!isset ($_SESSION['sesion_ventas_id'])) {
+          if (!isset ($_SESSION['sesion_ventas_id'])) {
            return "login";
           }
- */
+
         if(in_array($vista, $palabras_permitidas)){
             if (is_file("./views/".$vista.".php")) {
                 $contenido = "./views/".$vista.".php";
